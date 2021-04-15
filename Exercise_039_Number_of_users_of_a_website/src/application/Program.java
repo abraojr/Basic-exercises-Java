8fbeafeb0ababdaa718a886af5aafcfe04e53245
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-15 03:43:53 -0300

    Exercise 39: Number of users of a website

---
+++ Exercise_039_Number_of_users_of_a_website/src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import model.entities.LogEntry;
+
+public class Program {
+	
+	/*A website registers a log of users' access. A log record consists of the user name and the instant the user accessed the site in the ISO 8601
+     *standard, separated by space.Make a program that reads the access log from a file, and then inform how many different users have accessed the site.*/
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter file full path: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			Set<LogEntry> set = new HashSet<>();
+
+			String line = br.readLine();
+			while (line != null) {
+
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+
+				set.add(new LogEntry(username, moment));
+
+				line = br.readLine();
+			}
+			System.out.println("Total users: " + set.size());
+		} 
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+	}
+}
