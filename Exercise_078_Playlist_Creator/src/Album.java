b83eafb78f56c4f6279f0934e2976f2a32c5d7f1
Abrão Astério Junior <abraoastjr@gmail.com>
2026-03-17 14:17:04 -0300

    Exercise 78: Playlist Creator: Storing Songs & Custom Track Numbers

---
+++ Exercise_078_Playlist_Creator/src/Album.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+
+        int index = trackNumber - 1;
+
+        if ((index >= 0) && (index <= songs.size())) {
+            playlist.add(songs.get(index));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+
+        Song checkedSong = findSong(title);
+
+        if (checkedSong != null) {
+            playlist.add(checkedSong);
+            return true;
+        }
+
+        return false;
+    }
+
+    private Song findSong(String title) {
+
+        for (Song checkedSong : this.songs) {
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
+
+        return null;
+    }
+}
